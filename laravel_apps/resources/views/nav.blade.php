66ef9cdd57ecc080e0ce5bbf7db50588d3547660
Ojo Adebayo <bayoojo1@yahoo.com>
2020-08-24 16:33:17 +0100

    edit3

--- laravel_apps/resources/views/nav.blade.php
+++ laravel_apps/resources/views/nav.blade.php
@@ -7,13 +7,13 @@
         <div class="collapse navbar-collapse">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active" style="color: hotpink;">
-                    <a class="nav-link" href="/"><b>Home</b></a>
+                    <a class="nav-link" href="{{env('app_url')}}/"><b>Home</b></a>
                 </li>
                 <li class="nav-item" style="color: hotpink;">
-                    <a class="nav-link" href="/cats.html"><b>Cats</b></a>
+                    <a class="nav-link" href="{{env('app_url')}}/cats"><b>Cats</b></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/dogs.html"><b>Dogs</b></a>
+                    <a class="nav-link" href="{{env('app_url')}}/dogs"><b>Dogs</b></a>
                 </li>
 
             </ul>
